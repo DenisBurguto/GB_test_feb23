c243c0508fa9e0d56b694c3e55aa95f50c03fe11
«Denis <6545158@gmail.com>
2023-02-12 21:26:36 +0300

    Programs main method

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] GetArrayWithElementsNeededLength(string[] array, int elementLength)
+{
+    string[] outString = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= elementLength)
+        {
+            outString[j] = array[i];
+            j++;
+        }
+    }
+    array.Resize( ref outString, j);
+    return outString;
+
+}
